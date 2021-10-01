7182a551c7ee5f29602a1fe5bc1858884f6de8a4
Peter-Immanuel <peterbemshima@gmail.com>
2021-10-01 23:12:41 +0100

    NGO app finalized

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+
+function compute(){
+        var rate = document.getElementById("interest")
+        var principal = document.getElementById("principal")
+        var years = document.getElementById('years')
+
+        let date = new Date()
+
+        interest =parseInt((principal.value * years.value * (rate.value /100)))
+
+        document.getElementById("amt").innerHTML=principal.value
+        document.getElementById("rate").innerHTML = rate.value + "%"
+        document.getElementById("result").innerHTML = interest
+        document.getElementById("yrs").innerHTML =  date.getFullYear() + parseInt(years.value)    
 }
-        
+
+function getRange(){
+        var rate = document.getElementById("interest")
+        document.getElementById("range-value").innerHTML = rate.value+"%"
+}
